54fbec7e465aa25e1f04d2c8710f8c40963ade67
kowus <barnabas@elite-education.org>
2017-09-12 13:03:16 +0000

    added autocomplete search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -10,7 +10,7 @@ $(function () {
                     // Map response values to fiedl label and value
                     response($.map(data, function (el) {
                         return {
-                            label: el.fullname,
+                            label: el.name,
                             value: el._id
                         };
                     }));
@@ -19,7 +19,7 @@ $(function () {
         },
 
         // The minimum number of characters a user must type before a search is performed.
-        minLength: 3,
+        minLength: 1,
 
         // set an onFocus event to show the result on input field when result is focused
         focus: function (event, ui) {
@@ -33,6 +33,7 @@ $(function () {
             // Set the id to the next input hidden field
             $(this).next("input").val(ui.item.value);
             // Prevent other event from not being execute
+            window.location.replace("/products/id/"+ui.item.value);
             event.preventDefault();
             // optionnal: submit the form after field has been filled up
             $('#quicksearch').submit();
