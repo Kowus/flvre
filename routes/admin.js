86ba69f4389cc25cf8630aa45b271035e8568be7
Kowus <barnabas@elite-education.org>
2017-09-26 22:55:56 +0000

    fix tags reqs

--- routes/admin.js
+++ routes/admin.js
@@ -54,8 +54,6 @@ router.post('/add_item', needsGroup('admin'), function (req, res, next) {
     });
 });
 
-
-
 router.post('/upload/image', needsGroup('admin'), function(req, res, next) {
     console.log(req.files['images']);
     uploader.upload('s3', req.files['images'], function(err, files) {
